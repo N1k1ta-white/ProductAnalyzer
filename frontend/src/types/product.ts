bf2d0b6f7fa8cb54ca91311d5e5423ba060cec22
rlitvinov <litvinromaha@icloud.com>
2025-02-27 01:52:31 +0200

    restructurization

--- frontend/src/types/product.ts
+++ frontend/src/types/product.ts
@@ -1,4 +1,13 @@
-export interface Product {
+export interface Attribute {
+    name: string
+    value: string | number
+}
 
+export interface Product {
+    id: number,
+    name: string,
+    price: number,
+    category: string,
+    attributes: Attribute[]
 }
 
