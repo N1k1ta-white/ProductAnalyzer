293c26dbaed7c60eda7881785a8e79d44c1b5dac
Никита <65460812+N1k1ta-white@users.noreply.github.com>
2025-02-28 01:49:02 +0200

    Added JwtGuard for validating the given JWT token in the header and passing requests with a valid token

--- backend/gateway/src/app.module.ts
+++ backend/gateway/src/app.module.ts
@@ -2,12 +2,22 @@ import { Module } from '@nestjs/common';
 import { RabbitMqModule } from './rabbit-mq/rabbit-mq.module';
 import { GatewayController } from './controllers/gateway.controller';
 import { ConfigModule } from '@nestjs/config';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+import { APP_GUARD } from '@nestjs/core';
+import { JwtGuard } from './jwt.guard';
 
 @Module({
   imports: [RabbitMqModule,
     ConfigModule.forRoot(),
+    PassportModule.register({ defaultStrategy: 'jwt' })
   ],
   controllers: [GatewayController],
-  providers: [],
+  providers: [JwtStrategy, 
+    {
+      provide: APP_GUARD,
+      useClass: JwtGuard,
+    }
+  ],
 })
 export class AppModule {}
--- backend/gateway/src/controllers/gateway.controller.ts
+++ backend/gateway/src/controllers/gateway.controller.ts
@@ -1,9 +1,11 @@
-import { BadRequestException, Body, Controller, HttpStatus, Inject, Post, UnauthorizedException, UseFilters } from '@nestjs/common';
+import { BadRequestException, Body, Controller, Get, HttpStatus, Inject, Post, UnauthorizedException, UseFilters, Headers } from '@nestjs/common';
 import { ClientProxy } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
+import { Public } from 'src/public.decorator';
 
 const REGISTER_USER = "register"
 const AUTH_USER = "auth"
+const PROFILE_USER = "profile"
 
 @Controller()
 export class GatewayController {
@@ -12,20 +14,33 @@ export class GatewayController {
         @Inject('AUTH_SERVICE') private readonly authClient: ClientProxy,
     ) {}
 
+    // TODO: Implement handling exceptions 
+
+    @Public()
     @Post("register")
-    async register(@Body() user: any) {
+    async register(@Body() payload: any) {
         try {
-            const res = await firstValueFrom(this.authClient.send(REGISTER_USER, user))
+            const res = await firstValueFrom(this.authClient.send(REGISTER_USER, payload))
             return res
         } catch (error) {
             throw new BadRequestException(error.message)
         }
     }
 
+    @Public()
     @Post("login")
-    async login(@Body() user: any) {
+    async login(@Body() payload: any) {
         try {
-            return this.authClient.send(AUTH_USER, user)
+            return this.authClient.send(AUTH_USER, payload)
+        } catch (error) {
+            throw new UnauthorizedException(error.message)
+        }
+    }
+
+    @Get("profile")
+    async profile(@Headers('authorization') jwtToken: string) {
+        try {
+            return this.authClient.send(PROFILE_USER, jwtToken.replace('bearer ', ''))
         } catch (error) {
             throw new UnauthorizedException(error.message)
         }
