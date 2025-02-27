5344a59eb503de529bf51352f53f233f9698d98a
Никита <65460812+N1k1ta-white@users.noreply.github.com>
2025-02-27 22:53:57 +0200

    the database module is separated from app module

--- backend/auth/src/app.module.ts
+++ backend/auth/src/app.module.ts
@@ -6,19 +6,11 @@ import { UserService } from './sevices/user.service';
 import { JwtModule, JwtService } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
+import { DatabaseModule } from './database.module';
 
 @Module({
   imports:[ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: 3306,
-      username: process.env.MYSQL_USER || 'root',
-      password: process.env.MYSQL_PASSWORD || 'password',
-      database: process.env.MYSQL_DATABASE || 'mysql',
-      entities: [User],
-      synchronize: true,
-    }),
+    DatabaseModule,
     TypeOrmModule.forFeature([User]),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
---
+++ backend/auth/src/database.module.ts
@@ -0,0 +1,21 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { User } from './entities/user.entity';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST || 'localhost',
+      port: 3306,
+      username: process.env.MYSQL_USER || 'root',
+      password: process.env.MYSQL_PASSWORD || 'password',
+      database: process.env.MYSQL_DATABASE || 'mysql',
+      entities: [User],
+      synchronize: true,
+    }),
+  ],
+})
+export class DatabaseModule {}
